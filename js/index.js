c14d84a852d7822397c71241f1eabd39196a0c6c
MikalaiF <filmikalai@gmail.com>
2023-05-01 05:19:37 +0300

    fix: meta-date

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 const letters = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'delete'],
-['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', String.fromCharCode(92)],
-['caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', String.fromCharCode(39), 'return'],
-['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', String.fromCharCode(9650), 'shift'],
-['fn', String.fromCharCode(8743), '⌥', '⌘', String.fromCharCode(160), '⌘', String.fromCharCode(9664), String.fromCharCode(9660), String.fromCharCode(9654), '⌥']];
-//let letters = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 32];
+  ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', String.fromCharCode(92)],
+  ['capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', String.fromCharCode(39), 'return'],
+  ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', String.fromCharCode(9650), 'shiftR'],
+  ['fn', String.fromCharCode(8743), '⌥', '⌘', ' ', '⌘', String.fromCharCode(9664), String.fromCharCode(9660), String.fromCharCode(9654), '⌥']];
 
 function createWrapper() {
   const wrapper = document.createElement('div');
@@ -31,7 +30,9 @@ function createTexArea() {
 }
 
 container1.appendChild(createTexArea());
+// eslint-disable-next-line no-unused-vars
 const containerTextArea = document.querySelector('.containerTextArea');
+
 // ----------------------containerTextArea------------^
 
 function createDiv() {
@@ -41,71 +42,86 @@ function createDiv() {
 }
 
 container1.appendChild(createDiv());
+// eslint-disable-next-line no-unused-vars
 const containerKeyBoard = document.querySelector('.containerKeyBoard');
 // ----------------------containerKeyBoard------------^
 
+// eslint-disable-next-line no-unused-vars
 function createLetter() {
   const span = document.createElement('span');
-  span.className = 'letter';
   return span;
 }
 // ----------------------Letter------------^
 
-function createLine() {
+// eslint-disable-next-line no-unused-vars
+function createLine(l) {
   const line = document.createElement('div');
-  line.className = 'line';
+  line.className = `line${l}`;
   return line;
 }
 // ----------------------Line------------^
 
 (function createKeyboard() {
   let out = '';
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < letters.length; i++) {
-    containerKeyBoard.appendChild(createLine());
+    // eslint-disable-next-line no-plusplus
     for (let j = 0; j < letters[i].length; j++) {
-      const letter = containerKeyBoard.childNodes[i].appendChild(createLetter());
-      letter.innerHTML = letters[i][j];
-      if (letters[i][j] === 'delete') {
-        letter.classList.add('delete');
+      if (letters[i][j] === 'tab' || letters[i][j] === 'capslock' || letters[i][j] === 'shift' || letters[i][j] === 'fn') {
+        out += '<div class = "clearfix"></div>';
       }
-      if (letters[i][j] === 'tab') {
-        letter.classList.add('tab');
-      }
-      if (letters[i][j] === 'caps lock') {
-        letter.classList.add('caps_lock');
-      }
-      if (letters[i][j] === 'shift') {
-        letter.classList.add('shift');
-      }
-      if (letters[i][j] === String.fromCharCode(160)) {
-        letter.classList.add('space');
-      }
-      if (letters[i][j] === 'return') {
-        letter.classList.add('return');
-      }
-      if (letters[i][j] === '⌘') {
-        letter.classList.add('command');
-      }
-      if (letters[i][j] === String.fromCharCode(8743)) {
-        letter.classList.add('control');
-      }
-      if (letters[i][j] === '⌥') {
-        letter.classList.add('option');
-      }
-      if (letters[i][j] === 'fn') {
-        letter.classList.add('fn');
-      }
+      out += `<div class = " letter" data-id = "${letters[i][j]}">${letters[i][j]}</div>`;
+      const line = document.querySelector('.containerKeyBoard');
+      
+      line.innerHTML = out;
+      console.log(document.querySelector('[data-id]'));
     }
   }
 }());
 
 // ----------------------Create------------^
 
-//function init() {
-//  let out = '';
-//  for (let i = 0; i < letters.length; i++){
-//    out += `<div class="letter"> ${String.fromCharCode(letters[i])}</div>`;
-//  }
-//  containerKeyBoard.innerHTML = out;
+//document.onkeydown = function (event) {
+//  document.querySelectorAll('.containerKeyBoard .letter').forEach((el) => {
+//    el.classList.remove('active');
+//  });
+//  document.querySelector(`.containerKeyBoard .letter[data="${event.key}"]`).classList.add('active');
+//};
+
+//document.querySelectorAll('.containerKeyBoard .letter').forEach((el) => {
+//  // eslint-disable-next-line no-param-reassign
+//  el.onclick = function (event) {
+//    // eslint-disable-next-line no-shadow
+//    console.log(event);
+//    document.querySelectorAll('.containerKeyBoard .letter').forEach((el) => {
+//      el.classList.remove('active');
+//    });
+//    const code = el.getAttribute('data');
+//    this.classList.add('active');
+//    if (code === ' ') {
+//      this.classList.add('active');
+//    }
+//    if (code === String.fromCharCode(8743)) {
+//      this.classList.add('active');
+//    }
+//    console.log(code);
+//  };
+//});
+const dataLetters = Array.from(document.querySelectorAll('[data-id]'));
+console.log(dataLetters)
+
+function init2() {
+  containerTextArea.addEventListener('keydown', keyDownHandler);
+  //containerTextArea.addEventListener('keyup', keyUpHandler);
+}
+init2();
+
+function keyDownHandler(event) {
+  event.preventDefault();
+  let letter = dataLetters.find((x) => console.log(x.dataset))
+  console.log(letter)
+}
+
+//function keyUpHandler(event) {
+  
 //}
-//init()
